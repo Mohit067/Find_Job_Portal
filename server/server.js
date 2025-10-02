deae286405c10c4323d1756f8d785552ed02d1fd
Mohit sahu <mohitsahu60067@gmail.com>
2025-10-02 21:21:37 +0530

    added sentry for error handling and mongodb setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+import './config/instrument.js'
 import express from "express";
 import cors from 'cors'
 import connectDb from "./config/db.js";
 import dotenv from "dotenv";
+import * as Sentry from "@sentry/node"
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 // Initialize Express
 const app = express();
@@ -18,10 +21,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Api working")
 })
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
+
+app.post('/webhooks/', clerkWebhooks)
 
 // Port
 const PORT = process.env.PORT || 5000
+
+Sentry.setupExpressErrorHandler(app)
 app.listen(PORT, async () => {
     console.log(`app is running on http://localhost:${PORT}`)
 })
